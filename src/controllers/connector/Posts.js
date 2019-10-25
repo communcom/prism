1b131fa9c854563d34a70d043defb8b67a99b506
Ivan Chernyshev <tcherivan@gmail.com>
2019-10-25 19:28:51 +0300

    Feat: new feed sorting (#134)

--- src/controllers/connector/Posts.js
+++ src/controllers/connector/Posts.js
@@ -201,7 +201,7 @@ class Posts extends BasicController {
         }
 
         const paging = [{ $skip: offset }, { $limit: limit }];
-        const sort = { $sort: { 'meta.creationTime': 1 } };
+        const sort = { $sort: { 'meta.creationTime': -1 } };
 
         const items = await this._aggregate([
             filter,
@@ -228,7 +228,7 @@ class Posts extends BasicController {
         }
 
         const paging = [{ $skip: offset }, { $limit: limit }];
-        const sort = { $sort: { 'meta.creationTime': 1 } };
+        const sort = { $sort: { 'meta.creationTime': -1 } };
 
         const items = await this._aggregate([
             filter,
