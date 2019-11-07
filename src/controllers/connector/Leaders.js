41303dc7bd6d82e281529028acc9d8635b2fae8b
Alexey Elizarov <felex-ae@ya.ru>
2019-11-07 17:43:21 +0300

    fix: getLeaderCommunities isActive (#174)

--- src/controllers/connector/Leaders.js
+++ src/controllers/connector/Leaders.js
@@ -172,6 +172,7 @@ class Leaders extends BasicController {
             {
                 $match: {
                     userId,
+                    isActive: true,
                 },
             },
             {
