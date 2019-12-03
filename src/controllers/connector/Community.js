23c443145a0aec5aa037f8889697c306607c4a70
IvanBlacky <tcherivan@gmail.com>
2019-12-03 18:12:09 +0300

    Feat: exclude BOOKS community

--- src/controllers/connector/Community.js
+++ src/controllers/connector/Community.js
@@ -266,6 +266,9 @@ class Community extends BasicController {
             query.$and = [{ communityId: { $ne: 'PORN' } }, { communityId: { $ne: 'NSFW' } }];
         }
 
+        query.$and = query.$and || [];
+        query.$and.push({ communityId: { $ne: 'BOOKS' } });
+
         if (search) {
             query.communityName = { $regex: `^${escape(search.trim())}` };
         }
